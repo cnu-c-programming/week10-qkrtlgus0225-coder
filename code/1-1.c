f51d5615062bdb26a976ae9a278e5d5f34898354
qkrtlgus0225-coder <qkrtlgus0225@gmail.com>
2026-05-08 11:03:30 +0900

    Update 1-1.c

---
+++ code/1-1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+struct S1{
+char c;
+int i;
+long long l;
+};
+
+struct S2{
+  char c;
+  int i;
+  long long l;
+}__attribute__((packde));
+
+int main(){
+  struct S1 s1;
+  struct S2 s2;
+  print("%zu %zu\n", sizeof(s1), sizeof(s2)); 
+}
